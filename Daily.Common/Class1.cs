3275c9576e6d399e41c1b7910fd119094f34f36a
NGonzalez07 <ngonzalez@solindustrial.com.mx>
2025-05-22 17:57:49 -0600

    Add project files.

---
+++ Daily.Common/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Daily.Common
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Daily.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Daily.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
